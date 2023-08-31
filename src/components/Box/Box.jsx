080e88bd3e6bf81b1477fd9a659c4cf7bc757ec5
Amruta Marathe <amruta.marathe25@gmail.com>
2023-08-31 11:16:33 +1000

    added Board logic and styling

--- src/components/Box/Box.jsx
+++ src/components/Box/Box.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './Box.css'
 
 const Box = ({value,onClick}) => {
-    const styles = value === "x" ?"box x": "box o"
+    const styles = value === "X" ?"box X": "box O"
   return (
     <button className ={styles} onClick = {onClick}>{value}</button>
   )
